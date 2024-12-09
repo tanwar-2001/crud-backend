d26f6672ad616773a07e44ff33100c472aae9ca7
Mayank Tanwar <mayank@costacloud.com>
2024-12-09 12:17:36 +0530

    complete crud

---
+++ controllers/product.controller.js
@@ -0,0 +1,68 @@
+const Product = require("../models/product.model");
+
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const products = await Product.findById(id);
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).send(product);
+  } catch (error) {
+    console.log({ message: error.message });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      res
+        .status(404)
+        .json({ message: `Product with id ${id} not found in DB` });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      res
+        .status(404)
+        .json({ message: `Product with id ${id} not found in DB` });
+    }
+    res.status(200).json(product);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Product = require("./models/product.model");
+const productRoute = require("./routes/product.route");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose
   .connect(
@@ -15,15 +17,4 @@ mongoose
   })
   .catch(() => console.log("Error occured while connecting to the DB"));
 
-app.get("/", (req, res) => {
-  res.send("Hello from Node API");
-});
-
-app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).send(product);
-  } catch (error) {
-    console.log({ message: error.message });
-  }
-});
+app.use("/api/products", productRoute);
